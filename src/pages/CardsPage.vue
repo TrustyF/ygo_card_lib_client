e5e61119d6f9a8123d17cc9a5046cbc622f6bcc8
Trusty <sirjacobs.arthur@gmail.com>
2023-09-01 12:22:58 +0200

    created cardslist

--- src/pages/CardsPage.vue
+++ src/pages/CardsPage.vue
@@ -1,30 +1,15 @@
 <script setup>
 import {inject, onMounted, watch, ref, computed} from "vue";
-import CardMasterComponent from "../components/card/CardMasterComponent.vue";
+import CardList from "../components/card/CardList.vue";
 
 // let props = defineProps(["test"]);
 const curr_api = inject("curr_api");
 
-let user_cards = ref([])
 
-function get_all_cards() {
-  fetch(new URL(`${curr_api}/card/get_all`))
-      .then(response => response.json())
-      .then(data => {
-        user_cards.value = data
-        console.log('cards',data);
-      })
-}
-
-onMounted(() => {
-  get_all_cards()
-})
 </script>
 
 <template>
-  <div v-for="card in user_cards" :key="card.id">
-    <card-master-component :card="card"></card-master-component>
-  </div>
+<card-list></card-list>
 </template>
 
 <style scoped>
