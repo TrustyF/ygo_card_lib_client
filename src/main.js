0587207802b7e72cb601a48721e7c45bae636a6e
Arthur Sirjacobs <sirjacobs.arthur@gmail.com>
2025-09-01 18:38:22 -0700

    Making local and self-hosted

--- src/main.js
+++ src/main.js
@@ -1,18 +1,16 @@
 import '/src/assets/base.css'
 
-import {createApp, provide, ref} from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueLazyLoad from 'vue-lazyload'
 import LoadGif from '/cards/Card_loading.jpg'
 
-const local_api = "http://192.168.1.11:5000";
-const server_api = "https://ygo-TrustyFox.pythonanywhere.com";
-// const dev_mode = import.meta.env.DEV;
-const dev_mode = false;
+const local_api = "http://localhost:5000";
+const dev_mode = true;
 const debug_mode = import.meta.env.DEV;
-// const debug_mode = false
-const curr_api = dev_mode ? local_api : server_api
+
+const curr_api = local_api
 
 const app = createApp(App)
 
